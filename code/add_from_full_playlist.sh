a02bb85b9bc1e11797266ef99b7cd64dd9b64bd7
Yaroslav Halchenko <debian@onerussian.com>
2022-09-01 16:19:58 -0400

    prototype to get all prior videos (no metadata)

---
+++ code/add_from_full_playlist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+# json file, output folder
+# damn yarik -- do it in python
+
+json="$1"
+outfolder="$2"
+
+IFS="&"
+jq '.items[] |  .date_modified[:10] + "-" + .title + "_" + ".mkv" + "&" + .url' "$json" \
+| tr ' ' '_' \
+| sed -e 's,["],,g' -e 's,_*\.mkv,.mkv,g' -e 's,[:()!],_,g' \
+| while read f url; do 
+    url=${url//_/:}
+    if [ -L "$outfolder/$f" ]; then 
+        echo "$f exists $url"; 
+    else
+        git annex addurl --file $outfolder/$f --fast $url
+        # TODO: unlike rssfeed we would not get metadata!
+    fi
+done 
+
+echo "we are done with it -- removing the list"
+rm "$json"
