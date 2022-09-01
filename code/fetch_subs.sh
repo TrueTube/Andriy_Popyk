6b211d537ea81d8f379d1f2a19feea7a6a0d8396
Yaroslav Halchenko <debian@onerussian.com>
2022-09-01 18:51:12 -0400

    log which subtitles were tried to obtain so we do not retry over and over again
    some files are lacking any

--- code/fetch_subs.sh
+++ code/fetch_subs.sh
@@ -3,6 +3,10 @@
 set -eu
 
 # fetch subtitles for video file(s) if there is none
+mkdir -p .datalad/status
+subs_done=.datalad/status/fetched-subs.log
+touch "$subs_done"  # to >> or grep  at the beginning of the universe
+
 for f in "$@"; do
     fbase=${f%.*}
     vtts=$(/bin/ls -d "$fbase".*.vtt 2>/dev/null || :)
@@ -10,7 +14,12 @@ for f in "$@"; do
         echo "$fbase: already has some vtts" # : $vtts"
         continue
     fi
+    if grep -q "^'$f'" $subs_done; then
+        echo "$f: already was getting subs, might have none"
+        continue
+    fi
     url=$(git annex whereis --in web "$f" | awk '/^ *web:/{print $2;}')
     echo "$fbase: getting some for $url"
-    yt-dlp --write-subs --write-auto-subs -k --sub-lang=en,ua,ru --skip-download -o "$fbase" "$url"
+    yt-dlp --write-subs --write-auto-subs -k --sub-lang=en,ua,ru --skip-download -o "$fbase" "$url" \
+    && echo -e "$f\t$url" >> "$subs_done"
 done
