ffd52816c27f9f7fa05f92a9f42e5a124ddddce9
Yaroslav Halchenko <debian@onerussian.com>
2022-09-03 12:01:52 -0400

    adding config for feeds to update all

--- code/import_feed.sh
+++ code/import_feed.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 playlist_id="$1"
+# so if no path provided -- using feedtitle
+path="${2:-\${feedtitle\}}"
 
-git annex importfeed --fast "https://www.youtube.com/feeds/videos.xml?playlist_id=$playlist_id" --template '${feedtitle}/${itempubdate}-${itemtitle}.mkv'
+git annex importfeed --fast "https://www.youtube.com/feeds/videos.xml?playlist_id=$playlist_id" --template "${path%/}"'/${itempubdate}-${itemtitle}.mkv'
---
+++ code/update_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname $0)
+cd ..
+
+config() {
+    git config -f .datalad/config "$@"
+}
+
+git config -f .datalad/config -l | awk -F. '/^playlist\./{print $2}' | sort | uniq \
+| while read name; do
+   path=$(config playlist.$name.path 2>/dev/null || echo $name/)
+   id=$(config playlist.$name.id)
+   echo "Updating feed $name"
+   datalad run -m "Updated feed $name ($id) under $path" code/import_feed.sh "$id" "$path"
+done
