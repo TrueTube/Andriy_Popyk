fc17011d8b6feffd4f4e5bc4ea5e48592cdcd10a
Yaroslav Halchenko <debian@onerussian.com>
2024-01-31 14:22:26 -0500

    --force importfeed since we have multiple playlists but then remove 2_ files
    Ref: https://git-annex.branchable.com/forum/importfeed_on_multiple___34__overlapping__34___playlists/?updated

--- code/import_feed.sh
+++ code/import_feed.sh
@@ -4,4 +4,5 @@ playlist_id="$1"
 # so if no path provided -- using feedtitle
 path="${2:-\${feedtitle\}}"
 
-git annex importfeed --fast "https://www.youtube.com/feeds/videos.xml?playlist_id=$playlist_id" --template "${path%/}"'/${itempubdate}-${itemtitle}.mkv'
+git annex importfeed --force --fast "https://www.youtube.com/feeds/videos.xml?playlist_id=$playlist_id" --template "${path%/}"'/${itempubdate}-${itemtitle}.mkv'
+git rm -f "${path%/}"/2_*.mkv
