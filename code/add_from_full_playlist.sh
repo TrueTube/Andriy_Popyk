0dc9ca6b461b8a3d7aff2f6513e36b4dfe369673
Yaroslav Halchenko <debian@onerussian.com>
2025-03-28 07:04:49 -0400

    Add --no-raw to addurl invocations to prevent addition of urls without yt-dlp handling
    ref: https://git-annex.branchable.com/todo/yt-dlp__58___parse__47__handle___40__error__41_____34__Video_unavailable__34__/

--- code/add_from_full_playlist.sh
+++ code/add_from_full_playlist.sh
@@ -17,7 +17,7 @@ jq '.items[] |  .date_modified[:10] + "-" + .title + "_" + ".mkv" + "&" + .url' 
     if [ -L "$outfolder/$f" ]; then 
         echo "$f exists $url"; 
     else
-        git -c annex.alwayscommit=false annex addurl --file $outfolder/$f --fast $url
+        git -c annex.alwayscommit=false annex addurl --no-raw --file $outfolder/$f --fast $url
         # TODO: unlike rssfeed we would not get metadata!
     fi
 done 
